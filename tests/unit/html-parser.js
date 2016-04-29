62b203752ac5d70405cd52799c2a798711b15704
vitkarpov <viktor.s.karpov@gmail.com>
2016-04-29 09:54:52 +0300

    test html input refactoring

--- tests/unit/html-parser.js
+++ tests/unit/html-parser.js
@@ -2,52 +2,51 @@ var expect = require('chai').expect;
 var Parser = require('../../src/html-parser.js');
 
 describe('HTML Parser', function() {
-    it('#consumeWhitespaces', function() {
-        var input = '   ';
-        var p = new Parser(input);
-
-        expect(p.consumeWhitespaces()).to.equal(input);
+    ([
+        { method: 'consumeWhitespaces', tests: [
+            { input: '   ', expected: '   ' },
+            { input: '  foo', expected: '  ' }
+        ] },
+        { method: 'consumeText', tests: [
+            { input: 'hello, world', expected: 'hello, world' },
+            { input: '   fooo    bar</h1>', expected: '   fooo    bar' },
+            { input: '1111!!!!&&**$$$FR#</h1>', expected: '1111!!!!&&**$$$FR#' }
+        ] },
+        { method: 'consumeTagName', tests: [
+            { input: 'h1', expected: 'h1' },
+            { input: 'b', expected: 'b' },
+            { input: 'html', expected: 'html' },
+        ] }
+    ]).forEach(function(item) {
+        describe('#' + item.method, function() {
+            item.tests.forEach(function(test) {
+                it('consumes "' + test.input + '" and returns "' + test.expected + '"', function() {
+                    var p = new Parser(test.input);
+                    expect(p[item.method]()).to.equal(test.expected);
+                });
+            });
+        });
     });
-    it('#consumeText', function() {
-        var input = 'hello, world</b>';
-        var p = new Parser(input);
+    describe('#consumeAttr', function() {
+        it('consumes single attr and return ab object with its name and value', function() {
+            var input = 'class="foo"';
+            var p = new Parser(input);
 
-        expect(p.consumeText()).to.equal('hello, world');
+            var attr = p.consumeAttr();
+
+            expect(attr).to.have.property('name').and.equal('class');
+            expect(attr).to.have.property('value').and.equal('foo');
+        });
     });
-    it('#consumeTagName', function() {
-        var input = 'h1';
-        var p = new Parser(input);
+    describe('#consumeAttrs', function() {
+        it('consumes multiple attrs and returns hash map with names and values', function() {
+            var input = 'class="foo" data-key="bar">';
+            var p = new Parser(input);
 
-        expect(p.consumeTagName()).to.equal(input);
-    });
-    it('#consumeAttrName', function() {
-        var input = 'class';
-        var p = new Parser(input);
+            var attrs = p.consumeAttrs();
 
-        expect(p.consumeTagName()).to.equal(input);
-    });
-    it('#consumeAttrValue', function() {
-        var input = '"foo"';
-        var p = new Parser(input);
-
-        expect(p.consumeAttrValue()).to.equal('foo');
-    });
-    it('#consumeAttr', function() {
-        var input = 'class="foo"';
-        var p = new Parser(input);
-
-        var attr = p.consumeAttr();
-
-        expect(attr).to.have.property('name').and.equal('class');
-        expect(attr).to.have.property('value').and.equal('foo');
-    });
-    it('#consumeAttrs', function() {
-        var input = 'class="foo" data-key="bar">';
-        var p = new Parser(input);
-
-        var attrs = p.consumeAttrs();
-
-        expect(attrs).to.have.property('class').and.equal('foo');
-        expect(attrs).to.have.property('data-key').and.equal('bar');
+            expect(attrs).to.have.property('class').and.equal('foo');
+            expect(attrs).to.have.property('data-key').and.equal('bar');
+        });
     });
 });
