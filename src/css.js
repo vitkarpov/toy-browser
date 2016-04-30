e036b3b3e8a17759593a189369c0bd305fc58ea3
vitkarpov <viktor.s.karpov@gmail.com>
2016-04-30 11:08:22 +0300

    :star: let the css journey begin

--- src/css.js
+++ src/css.js
@@ -5,3 +5,60 @@ var Parser = function(css) {
 }
 
 Parser.prototype = Object.create(BaseParser.prototype);
+
+Parser.prototype.getRules = function() {
+    var rules = [];
+
+    while (!this._eof()) {
+        this.consumeWhitespaces();
+        rules.push(this.getRule());
+    }
+    return rules;
+};
+
+Parser.prototype.getRule = function() {
+    return {
+        selectors: this.consumeSelectors(),
+        declarations: this.consumeDeclarations()
+    }
+};
+
+Parser.prototype.consumeSelectors = function() {
+    var selectors = [];
+
+    while (this._getCurrentChar() !== '{') {
+        selectors.push(this.consumeSimpleSelector());
+        this.consumeWhitespaces();
+        assert(this._consumeCurrentChar(), ',');
+        this.consumeWhitespaces();
+    }
+
+    return selectors;
+};
+
+Parser.prototype.consumeSimpleSelector = function() {
+    var result = {
+        ids: [],
+        classes: [],
+        tags: [],
+        universal: false
+    };
+
+    while (this._getCurrentChar() !== ',') {
+        switch (this._consumeCurrentChar()) {
+            case '.':
+                result.classes.push(this.consumeWord());
+                break;
+            case '#':
+                result.ids.push(this.consumeWord());
+                break;
+            case '*'
+                result.universal = true;
+                break;
+            default:
+                result.tags.push(this.consumeWord());
+                break;
+        }
+    }
+    return result;
+};
